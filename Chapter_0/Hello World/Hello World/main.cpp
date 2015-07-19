4d5c342045d05c43f0de0f936644880c34837875
Jeff Seo <jhseo.sk@gmail.com>
2015-07-19 05:57:01 -0600

    hello world code

---
+++ Chapter_0/Hello World/Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Jeff Seo on 2015-07-19.
+//  Copyright (c) 2015 Jeff Seo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
